4d05ab31dcd098e4b0a22cbbd052bbb33fb17535
erturkalan <erturker1996@hotmail.com>
2021-09-16 13:35:14 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,7 +8,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(child: Text('Dicee')),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +17,39 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TextButton(
+      onPressed: () {
+        setState(() {
+          leftDiceNumber = Random().nextInt(6) + 1;
+          rightDiceNumber = Random().nextInt(6) + 1;
+        });
+      },
+      child: SizedBox.fromSize(
+        size: Size(double.infinity, double.infinity),
+        child: Row(
+          children: [
+            Expanded(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+            SizedBox(
+              width: 16.0,
+            ),
+            Expanded(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
